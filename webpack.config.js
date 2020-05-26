030566ed8ea5973b2e941146ce00395fb4e75590
certilremy <certilremy@gmail.com>
2020-05-25 23:03:36 -0400

    add htmlPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'game.js',
+    filename: 'main.js',
   },
-  plugins: [new CleanWebpackPlugin(['dist'])],
+  plugins: [
+    new CleanWebpackPlugin([
+      'dist',
+    ]),
+    new HtmlWebpackPlugin(),
+  ],
 };
