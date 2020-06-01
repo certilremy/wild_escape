c770b8af23b1601386a673845dde55f8d96f3e7c
certilremy <certilremy@gmail.com>
2020-06-01 18:46:23 -0400

    add jump and platfrom

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-plusplus */
+/* eslint-disable max-len */
 /* eslint-disable no-undef */
 import 'phaser';
 import gameOptions from '../config/gameOptions';
@@ -45,9 +48,94 @@ export default class GameScene extends Phaser.Scene {
       'player',
     );
     this.player.setGravityY(gameOptions.playerGravity);
+    this.physics.add.collider(
+      this.player,
+      this.platformGroup, () => {
+        if (!this.player.anims.isPlaying) {
+          this.player.anims.play('run');
+        }
+      },
+      null,
+      this,
+    );
 
-
+    this.physics.add.overlap(
+      this.player,
+      this.coinGroup, (player, coin) => {
+        this.tweens.add({
+          targets: coin,
+          y: coin.y - 100,
+          alpha: 0,
+          duration: 800,
+          ease: 'Cubic.easeOut',
+          callbackScope: this,
+          onComplete() {
+            this.coinGroup.killAndHide(coin);
+            this.coinGroup.remove(coin);
+          },
+        });
+      },
+      null,
+      this,
+    );
+    this.input.on('pointerdown', this.jump, this);
     // end create
+  }
+
+  addPlatform(platformWidth, posX, posY) {
+    this.addedPlatforms++;
+    let platform;
+    if (this.platformPool.getLength()) {
+      platform = this.platformPool.getFirst();
+      platform.x = posX;
+      platform.y = posY;
+      platform.active = true;
+      platform.visible = true;
+      this.platformPool.remove(platform);
+      const newRatio = platformWidth / platform.displayWidth;
+      platform.displayWidth = platformWidth;
+      platform.tileScaleX = 1 / platform.scaleX;
+    } else {
+      platform = this.add.tileSprite(posX, posY, platformWidth, 32, 'platform');
+      this.physics.add.existing(platform);
+      platform.body.setImmovable(true);
+      platform.body.setVelocityX(Phaser.Math.Between(gameOptions.platformSpeedRange[0], gameOptions.platformSpeedRange[1]) * -1);
+      this.platformGroup.add(platform);
+    }
+    this.nextPlatformDistance = Phaser.Math.Between(gameOptions.spawnRange[0], gameOptions.spawnRange[1]);
+
+
+    if (this.addedPlatforms > 1) {
+      if (Phaser.Math.Between(1, 100) <= gameOptions.coinPercent) {
+        if (this.coinPool.getLength()) {
+          const coin = this.coinPool.getFirst();
+          coin.x = posX;
+          coin.y = posY - 96;
+          coin.alpha = 1;
+          coin.active = true;
+          coin.visible = true;
+          this.coinPool.remove(coin);
+        } else {
+          const coin = this.physics.add.sprite(posX, posY - 96, 'coin');
+          coin.setImmovable(true);
+          coin.setVelocityX(platform.body.velocity.x);
+          coin.anims.play('rotate');
+          this.coinGroup.add(coin);
+        }
+      }
+    }
+  }
+
+  // end add platform
+  jump() {
+    if (this.player.body.touching.down || (this.playerJumps > 0 && this.playerJumps < gameOptions.jumps)) {
+      if (this.player.body.touching.down) {
+        this.playerJumps = 0;
+      }
+      this.player.setVelocityY(gameOptions.jumpForce * -1);
+      this.playerJumps++;
+      this.player.anims.stop();
+    }
   }
 }
 function resize() {
