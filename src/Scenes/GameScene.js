188802704c22da636de885422836511ca68f46e2
certilremy <certilremy@gmail.com>
2020-06-01 18:11:55 -0400

    add resize

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -15,3 +15,23 @@ export default class GameScene extends Phaser.Scene {
     this.add.image(400, 300, 'logo2');
   }
 }
+function resize() {
+  const canvas = document.querySelector('canvas');
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const windowRatio = windowWidth / windowHeight;
+  const gameRatio = game.config.width / game.config.height;
+  if (windowRatio < gameRatio) {
+    canvas.style.width = `${windowWidth}px`;
+    canvas.style.height = `${windowWidth / gameRatio}px`;
+  } else {
+    canvas.style.width = `${windowHeight * gameRatio}px`;
+    canvas.style.height = `${windowHeight}px`;
+  }
+}
+
+window.onload = () => {
+  window.focus();
+  resize();
+  window.addEventListener('resize', resize, false);
+};
--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,8 @@ export default {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  backgroundColor: 0x0c88c7,
+  physics: {
+    default: 'arcade',
+  },
 };
