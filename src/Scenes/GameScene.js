1e9618ee4a87c2e955acb25e829a8678da14d8c9
certilremy <certilremy@gmail.com>
2020-06-01 18:32:14 -0400

    add the platform and coin

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,18 +1,53 @@
 /* eslint-disable no-undef */
 import 'phaser';
-import logo from '../assets/logo.png';
+import gameOptions from '../config/gameOptions';
+import config from '../config/config';
+
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
   }
 
-  preload() {
-    this.load.image('logo2', logo);
-  }
-
   create() {
-    this.add.image(400, 300, 'logo2');
+    this.addedPlatforms = 0;
+    this.platformGroup = this.add.group({
+      removeCallback(platform) {
+        platform.scene.platformPool.add(platform);
+      },
+    });
+    this.platformPool = this.add.group({
+      removeCallback(platform) {
+        platform.scene.platformGroup.add(platform);
+      },
+    });
+    this.coinGroup = this.add.group({
+      removeCallback(coin) {
+        coin.scene.coinPool.add(coin);
+      },
+    });
+
+    this.coinPool = this.add.group({
+      removeCallback(coin) {
+        coin.scene.coinGroup.add(coin);
+      },
+    });
+
+    this.playerJumps = 0;
+    this.addPlatform(
+      config.width,
+      config.width / 2,
+      config.height * gameOptions.platformVerticalLimit[1],
+    );
+    this.player = this.physics.add.sprite(
+      gameOptions.playerStartPosition,
+      config.height * 0.7,
+      'player',
+    );
+    this.player.setGravityY(gameOptions.playerGravity);
+
+
+    // end create
   }
 }
 function resize() {
